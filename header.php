f2c72f6ed19695f44c77cd712f5cbaabb8185492
sandipghosh <sandipgh19@gmail.com>
2017-05-13 13:23:31 +0530

    Update in header

--- header.php
+++ header.php
@@ -1,11 +1,17 @@
+<?php 
+$directoryURI = $_SERVER['REQUEST_URI'];
+$path = parse_url($directoryURI, PHP_URL_PATH);
+$components = explode('/', $path);
+$first_part = $components[1];
+?>
 <ul class="nav">
-			<li class="active"><a href="index.php">Home</a></li>	<!-- 
-			<li><a href="registration.php">Donor Registration</a></li>             -->
-			<li><a href="requests.php">send Request</a></li><!-- 
+            <li class="<?php if ($first_part=="") {echo "active"; } else  {echo "noactive";}?>"><a href="index.php">Home</a></li>   <!-- 
+            <li><a href="registration.php">Donor Registration</a></li>             -->
+            <li class="<?php if ($first_part=="requests.php") {echo "active"; } else  {echo "noactive";}?>"><a href="requests.php">send Request</a></li><!-- 
             <li><a href="viewrequest.php">View Request</a></li> -->
-            <li><a href="camps.php">Camps</a></li><!-- 
+            <li class="<?php if ($first_part=="camps.php") {echo "active"; } else  {echo "noactive";}?>"><a href="camps.php">Camps</a></li><!-- 
             <li><a href="login.php">log In</a></li>
             <li><a href="search.php">Search</a></li> -->
-            <li><a href="contact.php">Contact Us</a></li>
-           	<li><a href="aboutus.php">About</a></li>
+            <li class="<?php if ($first_part=="contact.php") {echo "active"; } else  {echo "noactive";}?>"><a href="contact.php">Contact Us</a></li>
+            <li class="<?php if ($first_part=="aboutus.php") {echo "active"; } else  {echo "noactive";}?>"><a href="aboutus.php">About</a></li>
             </ul>
