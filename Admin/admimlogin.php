c7931f71932fbe677d0af218e2d4b80d7226eee0
Saikat Chakrabortty <saikatchakrabortty2@gmail.com>
2017-05-18 04:03:03 +0530

    Session variable conflict Issue Fixed || Admin Dashboard working fine

--- Admin/admimlogin.php
+++ Admin/admimlogin.php
@@ -121,13 +121,14 @@ if(isset($_POST["sbmt"]))
 	$q=mysqli_query($cn,$s);
 	$r=mysqli_num_rows($q);
 	$data=mysqli_fetch_array($q);
+	//print_r($data[3]);exit;
 	
 	
 	mysqli_close($cn);
 	if($r>0)
 	{
 		$_SESSION["username"]=$_POST["t1"];
-		$_SESSION["usertype"]=$data[2];
+		$_SESSION["usertype"]=$data[3];
 		$_SESSION['loginstatus']="yes";
 
 header("location:index.php");
--- Admin/left.php
+++ Admin/left.php
@@ -1,4 +1,8 @@
-<?php if(!isset($_SESSION)) {session_start();}  ?>
+<?php if(!isset($_SESSION)) {
+    session_start();
+    }
+    //print_r($_SESSION); exit;  
+    ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
