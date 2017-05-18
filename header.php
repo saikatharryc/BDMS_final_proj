63bbec6ffd5ad780738fe43c548d21b06d9fe846
sandipghosh <sandipgh19@gmail.com>
2017-05-18 22:12:45 +0530

    Remove donar login

--- header.php
+++ header.php
@@ -10,7 +10,7 @@ $first_part = $components[1];
             <li class="<?php if ($first_part=="requests.php") {echo "active"; } else  {echo "noactive";}?>"><a href="requests.php">send Request</a></li><!-- 
             <li><a href="viewrequest.php">View Request</a></li> -->
             <li class="<?php if ($first_part=="camps.php") {echo "active"; } else  {echo "noactive";}?>"><a href="camps.php">Camps</a></li>
-            <li><a href="login.php">log In</a></li>
+          <!--  <li><a href="login.php">log In</a></li>-->
             <li class="<?php if ($first_part=="contact.php") {echo "active"; } else  {echo "noactive";}?>"><a href="search.php">Search</a></li> 
             <li class="<?php if ($first_part=="contact.php") {echo "active"; } else  {echo "noactive";}?>"><a href="contact.php">Contact Us</a></li>
             <li class="<?php if ($first_part=="aboutus.php") {echo "active"; } else  {echo "noactive";}?>"><a href="aboutus.php">About</a></li>
